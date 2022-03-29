461163f9b98a696c66ed20e6da724152c494f827
Herry Prasetyo <herry081288@gmail.com>
2022-03-30 01:09:50 +0700

    slicing done

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,48 @@
+<ul>
+    <li class="menu-title">Main</li>
+
+    <li>
+        <a href="index.html" class="waves-effect">
+            <i class="dripicons-meter"></i>
+            <span> Dashboard <span class="badge badge-success badge-pill float-right">3</span></span>
+        </a>
+    </li>
+
+    <li class="has_sub">
+        <a href="javascript:void(0);" class="waves-effect"><i class="dripicons-briefcase"></i> <span> Data Master
+            </span>
+            <span class="menu-arrow float-right"><i class="mdi mdi-chevron-right"></i></span></a>
+        <ul class="list-unstyled">
+            <li><a href="#">Data Kategori</a></li>
+        </ul>
+    </li>
+
+    <li class="has_sub">
+        <a href="javascript:void(0);" class="waves-effect"><i class="dripicons-broadcast"></i> <span> Advanced UI
+            </span> <span class="menu-arrow float-right"><i class="mdi mdi-chevron-right"></i></span></a>
+        <ul class="list-unstyled">
+            <li><a href="advanced-alertify.html">Alertify</a></li>
+            <li><a href="advanced-rating.html">Rating</a></li>
+            <li><a href="advanced-nestable.html">Nestable</a></li>
+            <li><a href="advanced-rangeslider.html">Range Slider</a></li>
+            <li><a href="advanced-sweet-alert.html">Sweet-Alert</a></li>
+            <li><a href="advanced-lightbox.html">Lightbox</a></li>
+        </ul>
+    </li>
+
+
+
+    <li class="has_sub">
+        <a href="javascript:void(0);" class="waves-effect"><i class="dripicons-list"></i><span> Tables </span> <span
+                class="menu-arrow float-right"><i class="mdi mdi-chevron-right"></i></span></a>
+        <ul class="list-unstyled">
+            <li><a href="tables-basic.html">Basic Tables</a></li>
+            <li><a href="tables-datatable.html">Data Table</a></li>
+            <li><a href="tables-responsive.html">Responsive Table</a></li>
+            <li><a href="tables-editable.html">Editable Table</a></li>
+        </ul>
+    </li>
+
+
+
+</ul>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <title>Drixo - Responsive Booststrap 4 Admin & Dashboard</title>
+    <meta content="Admin Dashboard" name="description" />
+    <meta content="ThemeDesign" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+    @include('components.css')
+</head>
+
+
+<body class="fixed-left">
+
+    <!-- Loader -->
+    <div id="preloader">
+        <div id="status">
+            <div class="spinner"></div>
+        </div>
+    </div>
+
+    <!-- Begin page -->
+    <div id="wrapper">
+
+        <!-- ========== Left Sidebar Start ========== -->
+        <div class="left side-menu">
+            <button type="button" class="button-menu-mobile button-menu-mobile-topbar open-left waves-effect">
+                <i class="ion-close"></i>
+            </button>
+
+            <div class="left-side-logo d-block d-lg-none">
+                <div class="text-center">
+
+                    <a href="index.html" class="logo"><img src="assets/images/logo-dark.png" height="20"
+                            alt="logo"></a>
+                </div>
+            </div>
+
+            <div class="sidebar-inner slimscrollleft">
+
+                <div id="sidebar-menu">
+                    @include('components.sidebar')
+                </div>
+                <div class="clearfix"></div>
+            </div> <!-- end sidebarinner -->
+        </div>
+        <!-- Left Sidebar End -->
+
+        <!-- Start right Content here -->
+
+        <div class="content-page">
+            <!-- Start content -->
+            <div class="content">
+
+                <!-- Top Bar Start -->
+                @include('components.topbar')
+                <!-- Top Bar End -->
+
+                <div class="page-content-wrapper ">
+
+                    @yield('container-fluid')
+                    <!-- container fluid -->
+
+                </div> <!-- Page content Wrapper -->
+
+            </div> <!-- content -->
+
+            @include('components.footer')
+
+        </div>
+        <!-- End Right content here -->
+
+    </div>
+    <!-- END wrapper -->
+
+
+    @include('components.js')
+    @yield('js')
+</body>
+
+</html>
