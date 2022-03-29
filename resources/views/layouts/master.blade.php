1fe5abd2e49798ac73f9dad0f0f7df551fc5b8bb
Herry Prasetyo <herry081288@gmail.com>
2022-03-30 01:30:51 +0700

    yo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,9 +5,9 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-    <title>Drixo - Responsive Booststrap 4 Admin & Dashboard</title>
-    <meta content="Admin Dashboard" name="description" />
-    <meta content="ThemeDesign" name="author" />
+    @yield('title')
+    <meta content="point of sale" name="description" />
+    <meta content="point of sale" name="Herry Prasetyo 2022" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
     <link rel="shortcut icon" href="assets/images/favicon.ico">
@@ -81,7 +81,7 @@
 
 
     @include('components.js')
-    @yield('js')
+    @yield('script')
 </body>
 
 </html>
